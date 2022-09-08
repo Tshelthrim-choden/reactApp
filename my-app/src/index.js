e0c6f17f3584d01ba272b73d943d259040044ca6
Tshelthrim-choden <110521502+Tshelthrim-choden@users.noreply.github.com>
2022-09-08 15:38:34 +0600

    Delete my-app directory

--- my-app/src/index.js
+++
@@ -1,8 +0,0 @@
-import ReactDom from 'react-dom/client';
-import React from 'react';
-import App from './App'
-
-const root = ReactDom.createRoot(document.getElementById('root'));
-root.render(<App></App>)
-
-
