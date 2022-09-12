18447e71238da734018288e894acd3048623a6a1
Tshelthrim-choden <tsheltrim@tshelthrim-aspire-e5-575.router.selise.dev>
2022-09-12 18:38:25 +0600

    new

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import ExpensesList from './ExpensesList';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesChart from './ExpensesChart';
 import './Expenses.css';
 
 function Expenses(props) {
@@ -35,6 +36,7 @@ function Expenses(props) {
       )}
        */}
    {/* {expensesContent} */}
+   <ExpensesChart expenses={filteredExpenses}/>
    <ExpensesList items={filteredExpenses} />
      </Card>
      </div>
